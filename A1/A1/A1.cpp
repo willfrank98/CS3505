50a691c9dd5fecfa644f5b960afb58d7d39252ae
Will Frank <coolwillrocks@gmail.com>
2017-08-28 12:46:55 -0600

    implemented updatePopulations and plotCharacter

--- A1/A1/A1.cpp
+++ A1/A1/A1.cpp
@@ -2,12 +2,17 @@
 //
 
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+//Calculates the updated population numbers after one iteration
 void updatePopulations(double g, double p, double c, double m, double K, double& numRabbits, double& numFoxes);
+
+//
 void plotCharacter(int spaces, char character);
-void chartingFunction(/*implement*/);
+void chartingFunction(double numRabbits, double numFoxes, double scaleFactor);
+void incrementCounter(int* counter);
 
 int main()
 {
@@ -25,19 +30,66 @@ int main()
 		cout << "Invalid input" << endl;
 	}
 
+	double g = 0.2;		//rabbitGrowth
+	double p = 0.002;	//predationRate
+	double c = 0.6;		//foxPreyConversion
+	double m = 0.2;		//foxMortalityRate
+	double K = 1000.0;	//carryCapacity
+
+
+	for (int i = 0; i < 500; i++)
+	{
+		updatePopulations(g, p, c, m, K, numRabbits, numFoxes);
+
+		if (numRabbits < 1 || numFoxes < 1)
+		{
+			//end simulation
+		}
+	}
+
 
     return 0;
 }
 
+/**
+	
+*/
 void updatePopulations(double g, double p, double c, double m, double K, double& numRabbits, double& numFoxes)
 {
+	double deltaRabbit = ((g * numRabbits) * (1 - (numRabbits / K))) - (p * numRabbits * numFoxes);
+	double deltaFoxes = (c * p * numRabbits * numFoxes) - (m * numFoxes);
 
+	numRabbits += deltaRabbit;
+	numFoxes += deltaFoxes;
 }
 
 void plotCharacter(int spaces, char character)
 {
+	string whiteSpace = "";
+
+	for (int i = 0; i < spaces; i++)
+	{
+		whiteSpace += " ";
+	}
+
+	whiteSpace += character;
+
+	cout << whiteSpace << endl;
 }
 
-void chartingFunction()
+void chartingFunction(double numRabbits, double numFoxes, double scaleFactor)
 {
+	//cast to ints
+	int adjustedFoxes = numFoxes * scaleFactor;
+	int adjustedRabbits = numRabbits * scaleFactor;
+
+	if (adjustedFoxes == adjustedRabbits)
+	{
+		
+	}
+}
+
+void incrementCounter(int* counter)
+{
+	counter++;
 }
