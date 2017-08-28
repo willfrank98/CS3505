bfc28276ed0cb628bc079686af369e0f29931173
Will Frank <willfrank98@gmail.com>
2017-08-27 23:35:07 -0600

    some more stuff

--- A1/A1/A1.cpp
+++ A1/A1/A1.cpp
@@ -1,18 +1,43 @@
 // A1.cpp : Defines the entry point for the console application.
 //
 
-#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
 
 void updatePopulations(double g, double p, double c, double m, double K, double& numRabbits, double& numFoxes);
+void plotCharacter(int spaces, char character);
+void chartingFunction(/*implement*/);
 
 int main()
 {
-    return 0;
+	double numRabbits, numFoxes;
 
+	cout << "Please enter an initial rabbit population: ";
+	if (!(cin >> numRabbits))
+	{
+		cout << "Invalid input" << endl;
+	}
+
+	cout << "Please enter an initial fox population: ";
+	if (!(cin >> numFoxes))
+	{
+		cout << "Invalid input" << endl;
+	}
+
+
+    return 0;
+}
+
+void updatePopulations(double g, double p, double c, double m, double K, double& numRabbits, double& numFoxes)
+{
 
 }
 
-void updatePopulations(double g, double p, double c, double m, double K, double & numRabbits, double & numFoxes)
+void plotCharacter(int spaces, char character)
 {
+}
 
+void chartingFunction()
+{
 }
